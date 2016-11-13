69066a9445f827ba1d94d7b5dcf75f704b9f23f1
Kyle Crichton <kylecrichton@gmail.com>
2016-11-13 17:29:52 -0600

    Finished javascript and added card images

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,38 +1,57 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-//if (cardTwo === cardFour) {
-//	alert('Sorry, try again.')
-//}
-//else if (cardOne === cardTwo) {
-//	alert('You found a match!')
-//}
-//else if (cardThree === cardFour) {
-//	alert('You found a match!')
-//}
-//else if (cardOne === cardThree) {
-//	alert('Sorry, try again.')
-//}
-//else if (cardOne === cardFour) {
-//	alert('Sorry, try again.')
-//}
-//else if (cardTwo === cardThree) {
-//	alert('Sorry, try again.')
-//}
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
+	  var cardElement = document.createElement('div');
+      cardElement.className = 'card';
+      cardElement.setAttribute('data-card', cards[i]);
+      cardElement.addEventListener('click', isTwoCards);
+	  board.appendChild(cardElement);
+	  board.appendChild(cardElement);
+	}
+}
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='images/king.png'>";
+	} else {
+		this.innerHTML = "<img src='images/queen.png'>";
+	}
+	
+	if (cardsInPlay.length === 2) {
+		setTimeout(function() {
+		isMatch(cardsInPlay);
+	}, 300)
+		cardsInPlay = [];
+		
+	}
+}
 
 //set board to variable
 var createCards = function() {
 
-var gameBoard = document.getElementById('game-board');
 
     for (i=0; i<4; i++) {
-        var cardDiv = document.createElement('div');
-        cardDiv.className = 'card';
-        gameBoard.appendChild(cardDiv);
-    }
+         var cardDiv = document.createElement('div');
+         cardDiv.className = 'card';
+         gameBoard.appendChild(cardDiv);
+     }
 }
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You're a winner!");
+	}
+	else {
+		alert("Oopsie.");
+	}
+}
+createBoard();
 createCards();
+
+
 
 
